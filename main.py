46a8df3cd29162732b8042a7216e35e02dad7003
Tomasz Madej <tomasz@madej.ovh>
2022-09-25 17:45:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,3 @@ for shop, stuff in list(lista.items()):
         stuff_up.append(y)
         things = things + 1
 
-    print('Idę do', shop.capitalize(), f'i kupuję tu następujące rzeczy: {stuff_up}.')
-print(f'W sumie kupuję {things} produktów.')
-print('')
